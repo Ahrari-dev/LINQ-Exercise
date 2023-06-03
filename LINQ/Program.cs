e61958395776cfac9a4344b7602e2530620e7d34
Hasib Ahrari <hasibahrari@gmail.com>
2023-06-03 15:38:28 -0700

    Final Commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<string> gameNames = new List<string>()
         {
-            Console.WriteLine("Hello, World!");
+            "Minecraft",
+            "Fortnite",
+            "Call of Duty",
+            "The Legend of Zelda",
+            "Final Fantasy",
+            "Super Mario Odyssey",
+            "Assassin's Creed",
+            "Grand Theft Auto",
+            "Red Dead Redemption",
+            "Overwatch"
+        };
+
+        Console.WriteLine("Original List:");
+        foreach (string game in gameNames)
+        {
+            Console.WriteLine(game);
         }
+
+        Console.WriteLine("\nOrdered List by Length of Game Name:");
+        List<string> orderedGameNames = gameNames.OrderBy(game => game.Length).ToList();
+        foreach (string game in orderedGameNames)
+        {
+            Console.WriteLine(game);
+        }
+
+        Console.ReadLine();
     }
 }
